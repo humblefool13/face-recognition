5f9e7b3d6a137d4f6f12de0942c2f024f22fee0d
humblefool13 <akshatksyp13@gmail.com>
2024-11-21 01:15:42 +0530

    changes

--- proxy.js
+++ proxy.js
@@ -1,14 +1,15 @@
-async function sendRequest(req) {
+export default async function sendRequest(req) {
+  console.log(req.headers);
   const request = await fetch(
     "https://3t2mxdqyx1.execute-api.ap-south-1.amazonaws.com/default/fr",
     {
       method: req.method,
-      headers: req.headers,
+      headers: {
+        "content-type": "application/json",
+      },
       body: JSON.stringify(req.body),
     }
   );
   const response = await request.json();
   return response;
 }
-
-module.exports = sendRequest;
